a6b61aa6d9c2cbd92cab6895669948a748dce481
Sungwoo Hong <sungwoohong@Sungwoos-MacBook-Pro.local>
2022-06-18 15:21:16 +0900

    every day commit day-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
+/* eslint-disable */
+
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import article from './article'
+
+let title = "my first keyboard"
+let summary = "I buy new keyboard. including typing video"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <Title />
+        <Article />
+      </ul>
     </div>
   );
-}
+};
+
+function Title() {
+  return (
+    <div className='title'>
+      <h4 className='titleH4'>DEVlog</h4>
+    </div>
+  );
+};
+
+
+function Article() {
+  return (
+    <div className='article'>
+      <Link to='/article'>
+        <h4>{title}</h4>
+        <p>{summary}</p>
+      </Link>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/article.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+import React from "react";
+import { Link, RouteProps } from 'react-router-dom';
+
+function Article() {
+    return (
+        <div>
+            안녕하세요
+        </div>
+    )
+}
+
+export default Article;
